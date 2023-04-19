8723d7980d88574de70f8d2b7996d398c4d3bb24
jreckin <>
2023-04-19 05:43:54 -0500

    Remove moved

--- GBB_Games/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-
-    site = r'https://www.wissports.net/schedule/day/league_instance/173330/~DATE~?subseason=827150&referrer=7291983'
-    game = r'https://www.wissports.net/game/show/~GAME_ID~?subseason=827150'
-    final_file = r'C:\Users\Jake\Documents\GitHub\DataPlayGround2\csv\WIS_GBB_GAME_DATA\~DATE~.csv'
-    
